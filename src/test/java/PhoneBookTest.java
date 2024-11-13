52cb1dec702916b80251da6d37c284d36593909c
Marina Malysheva <yumar1420@gmail.com>
2024-11-13 20:26:25 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+
+    @Test
+    void testAdding() {
+        String name = "John", number = "89534563212";
+        int expectedCount = 1;
+
+        int countAdding = phoneBook.add(name, number);
+
+        Assertions.assertEquals(expectedCount, countAdding);
+    }
 }
